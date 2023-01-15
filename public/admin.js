0e9df81a41cd2a0bcdc2c9e62d6810279f662ab3
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-01-15 07:53:13 -0500

    updated activity in class

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
+//get all your books
+async function main() {
+ const response = await fetch('http://localhost:3001/listBooks')
+ const books = await response.json()
 
-// Your Code Here
+ books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    console.log(book)
+    const root = document.getElementById('root')
+
+    const li = document.createElement('li')
+    li.textContent = book.title
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+
+    const saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        const body = {
+            id: book.id,
+            quantity: input.value
+        }
+        
+      fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+    })
+
+    li.append(input, saveButton)
+
+    li.append(input)
+
+    root.appendChild(li)
+}
+
+main()
